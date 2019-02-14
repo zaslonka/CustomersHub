57affb1987304edcb03ae8f20a45ae84dc4040b7
Konrad Zaslonka <zaslonka@gmail.com>
2019-02-14 08:23:59 +0100

    Implemented generic LocalMemory repository

---
+++ CustomersHub/CustomersHub.DataAccess.LocalMemory/LocalMemoryRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using CustomersHub.Core.Models;
+
+namespace CustomersHub.DataAccess.LocalMemory
+{
+    public class LocalMemoryRepository<T> where T : BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public LocalMemoryRepository()
+        {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.Id == t.Id);
+
+            if (tToUpdate != null)
+            {
+                tToUpdate = t;
+            }
+            else
+            {
+                throw new Exception(className + " Not found");
+            }
+        }
+
+        public T Find(string Id)
+        {
+            T t = items.Find(i => i.Id == Id);
+            if (t != null)
+            {
+                return t;
+            }
+            else
+            {
+                throw new Exception(className + " Not found");
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+        
+    }
+}
