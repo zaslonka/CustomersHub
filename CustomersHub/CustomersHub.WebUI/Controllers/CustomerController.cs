ebf210d49c084f24f57b42d4983576ba28c12644
Konrad Zaslonka <zaslonka@gmail.com>
2019-02-14 09:36:46 +0100

    Creating CustomerController and basic views

---
+++ CustomersHub/CustomersHub.WebUI/Controllers/CustomerController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomersHub.Core.Models;
+using CustomersHub.DataAccess.LocalMemory;
+
+namespace CustomersHub.WebUI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly LocalMemoryRepository<Customer> context;
+
+        public CustomerController()
+        {
+            context = new LocalMemoryRepository<Customer>();
+        }
+
+        public ActionResult Index()
+        {
+            List<Customer> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Customer product = new Customer();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(customer);
+            }
+            else
+            {
+                context.Insert(customer);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Customer customer = context.Find(Id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customer customer, string Id)
+        {
+            Customer customerToEdit = context.Find(Id);
+
+            if (customerToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(customer);
+                }
+
+                customerToEdit.City = customer.City;
+                customerToEdit.Status = customer.Status;
+                customerToEdit.Country = customer.Country;
+                customerToEdit.Email = customer.Email;
+                customerToEdit.Name = customer.Name;
+                customerToEdit.Phone = customer.Phone;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
